83ebcacd658649075704391e1c5c0b61529379a1
alberto-contreras <contreras.m.alberto@gmail.com>
2019-11-17 12:49:31 +0100

    First commit

---
+++ app/src/main/java/com/example/myfirstapplication_calculator/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myfirstapplication_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Asign a variable to each button
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
